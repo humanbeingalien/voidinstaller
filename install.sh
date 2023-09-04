b8ab2eb609b3858c470e002e813211c541715d1e
humanbeingalien <144038944+humanbeingalien@users.noreply.github.com>
2023-09-04 21:28:12 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+sudo xbps-install -Sy base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel xorg git kitty fish-shell sxhkd neovim rofi feh alsa-utils libxcb-devel xcb-proto xcb-util-devel xcb-util-wm-devel xcb-util-cursor-devel xcb-util-keysyms-devel nix elogind dbus gvfs udiskie
+
+
