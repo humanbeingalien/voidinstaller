86f98e13b9ef00b1808d216d4e5e091f3d99ab35
humanbeingalien <144038944+humanbeingalien@users.noreply.github.com>
2023-09-10 14:17:49 +0700

    Delete config directory

--- config/dk/scripts/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xsetroot -cursor_name left_ptr &
-xset r rate 350 35 &
-xrandr --output DVI-0 --mode 1440x900_60.00 &
-nitrogen --restore
-picom &
-
--- config/fish-0/conf.d/nix.fish
+++
@@ -1,2 +0,0 @@
-
-if test -e /home/ten/.nix-profile/etc/profile.d/nix.fish; . /home/ten/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
